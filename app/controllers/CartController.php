f78b209b94f9e38752ac5b16f278c943a8a57091
Tirsel Fedor <fedor.tirsel@sap.com>
2022-05-10 16:00:31 +0200

    Refactored checkout page

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -12,6 +12,10 @@
         private $user;
         private $product;
         private $cart;
+        private $promo_Codes = array(
+            'BLACKFRIDAY22' => '-5',
+            'BIGSALE'       => '-10%'
+        );
 
         public function __construct() 
         {
@@ -83,33 +87,58 @@
             exit(json_encode($payload));
         }
 
-        public function computeCartCost(array $cart_Records)
+        public function computeCartCost(array $cart_Records, string $promo_Code = null)
         {
             $total_Cost = 0;
             $i = 0;
 
-            if (gettype($cart_Records) === 'array' && count($cart_Records)) {
+            if (is_array($cart_Records) && count($cart_Records)) {
                 foreach ($cart_Records as $key => $value) {
                     $total_Cost += (float) $this->product->getSingleProductPrice($key) * (int) $value;
+                }
+            }
+
+            if (array_key_exists($promo_Code, $this->promo_Codes)) {
+                if (strpos($this->promo_Codes[$promo_Code], '%') > 0) {
+                    $total_Cost *= (100 + intval($this->promo_Codes[$promo_Code])) / 100;
+                } else {
+                    $total_Cost += intval($this->promo_Codes[$promo_Code]);
                 }
             }
         
             return round((float) $total_Cost, 2);
         }   
 
-         public function summary()
-         {    
-            $all_Cart_Records = $this->cart->getAllCartRecords(); 
-            $total_Cart_Cost = $this->computeCartCost($all_Cart_Records); 
-            
-            $this->render("summary", [
-                'title'     => 'Cart Summary', 
-                'cart_cost' => $total_Cart_Cost
-            ]);
-         }
+        public function summary()
+        {    
+            $all_Cart_Records = $this->cart->getAllCartRecords();
+            $count_Carts = $this->cart->countAllCarts();
+            $cart_Products = [];
 
-         public function checkout(int $retType, int $product_Id, string $pick_up_type)
-         {
+            if ($count_Carts > 0) {   
+                foreach ($all_Cart_Records as $key => $value) {
+                    $cart_Products[] = $this->product->getSingleProduct($key);
+                } 
+            }
+            
+            $applied_Promo_Code = array_key_first($this->promo_Codes);
+            $applied_Promo_Value = $this->promo_Codes[$applied_Promo_Code];
+
+            $all_Product_Records = $this->product->getAllProducts();
+            $total_Cart_Cost = $this->computeCartCost($all_Cart_Records, $applied_Promo_Code);
+
+            $this->render("summary", [
+                'title'               => 'Cart Summary', 
+                'all_cart_products'   => $cart_Products, 
+                'all_cart_quantity'   => $all_Cart_Records,
+                'applied_promo_code'  => $applied_Promo_Code,
+                'applied_promo_value' => $applied_Promo_Value,
+                'cart_cost'           => $total_Cart_Cost
+            ]);
+        }
+
+        public function checkout(int $retType, int $product_Id, string $pick_up_type)
+        {
             $all_Cart_Records = $this->cart->getAllCartRecords();
             $total_Cart_Cost = (gettype($all_Cart_Records) === 'array') ? $this->computeCartCost($all_Cart_Records) : 0;
              
@@ -126,7 +155,7 @@
                 'thank_you_message' => '<i class="fa fa-check"></i> We just received your order. You\'ll hear from us shortly.<br/> Thank you once again.', 
                 'cart_cost'         => $total_Cart_Cost
             ]);
-         }
+        }
     }
 
 ?>
--- app/views/summary.php
+++ app/views/summary.php
@@ -1,9 +1,257 @@
+<?php
+    
+
+?>
+
 <div class="demo_shopping-cart-wrapper">
     <div class="container">
         <div class="demo_listing-title">
             <h2>CART SUMMARY</h2>
         </div>
 
+        <div class="row">
+            <div class="col-md-4 order-md-2 mb-4">
+                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                    <span class="text-muted">Order recap</span>
+                    <span class="badge badge-secondary badge-pill"><?= count($data["all_cart_products"]) ?></span>
+                </h4>
+
+                <ul class="list-group mb-3 demo_cart-summary-list">
+                    <?php foreach ($data["all_cart_products"] as $cart): ?>
+                        <li class="list-group-item d-flex justify-content-between lh-condensed demo_cart-summary-list-item" id="cart-<?= $cart["id"]; ?>">
+                            <div>
+                                <h6 class="my-0"><?= $cart["name"] . " ({$data["all_cart_quantity"][$cart["id"]]}&times;)"; ?></h6>
+                                <small class="text-muted"><?= $cart["caption"] ?></small>
+                            </div>
+                            <span class="text-muted"><?= (float) $cart["price"] * $data["all_cart_quantity"][$cart["id"]] . '&nbsp;' . EURO; ?></span>
+                        </li>
+                    <?php endforeach; ?>
+
+                    <?php if (isset($data["applied_promo_code"]) && isset($data["applied_promo_value"])): ?>
+                        <li class="list-group-item d-flex justify-content-between bg-light">
+                            <div class="text-success">
+                                <h6 class="my-0">Promo code</h6>
+                                <small><?= $data["applied_promo_code"] ?></small>
+                            </div>
+                            <span class="text-success"><?= $data["applied_promo_value"] ?>&nbsp;<?= strpos($data["applied_promo_value"], '%') > 0 ? '%' : EURO ?></span>
+                        </li>
+                    <?php endif; ?>
+
+                    <li class="list-group-item d-flex justify-content-between">
+                        <span>Total Price (<?= EURO; ?>)</span>
+                        <strong><?= $data["cart_cost"] . '&nbsp;' . EURO; ?></strong>
+                    </li>
+                </ul>
+
+                <form class="card p-2">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Promo code">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-secondary">Redeem</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-md-8 order-md-1">
+                <h4 class="mb-3">Billing address</h4>
+                
+                <form class="needs-validation" novalidate>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="firstName"><b>First name *</b></label>
+                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                            <div class="invalid-feedback">
+                                Valid first name is required.
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="lastName"><b>Last name *</b></label>
+                            <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                            <div class="invalid-feedback">
+                                Valid last name is required.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="phoneNumber"><b>Phone number *</b></label>
+                            <input type="tel" class="form-control" id="phoneNumber" placeholder="123-456-7890" value="" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                            <div class="invalid-feedback">
+                                Valid phone number is required.
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="email"><b>Email address</b></label>
+                            <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                            <div class="invalid-feedback">
+                                Please enter a valid email address for shipping updates.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="address"><b>Address *</b></label>
+                        <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+                        <div class="invalid-feedback">
+                            Please enter your shipping address.
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="address2"><b>Address 2</b></label>
+                        <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-5 mb-3">
+                            <label for="country"><b>Country *</b></label>
+                            <select class="custom-select d-block w-100" id="country" required>
+                                <option value="" disabled selected hidden>Please select the country</option>
+                                <option>United Kingdom</option>
+                                <option>United States</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please select a valid country.
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 mb-3">
+                            <label for="zip"><b>Zip *</b></label>
+                            <input type="text" class="form-control" id="zip" placeholder="" required>
+                            <div class="invalid-feedback">
+                                Zip code required.
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <hr class="mb-4">
+                    
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="same-address">
+                        <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="save-info">
+                        <label class="custom-control-label" for="save-info">Save this information for next time</label>
+                    </div>
+                    
+                    <hr class="mb-4">
+
+                    <h4 class="mb-3">Shipping</h4>
+
+                    <div class="d-block my-3">
+                        <div class="custom-control custom-radio">
+                            <input id="pick-up" name="shippingType" value="PickUp" type="radio" class="demo_shipping-type custom-control-input" checked required>
+                            <label class="custom-control-label" for="pick-up">Pick Up (<?= PICK_UP_COST . ' ' . EURO ?>)</label>
+                        </div>
+
+                        <div class="custom-control custom-radio">
+                            <input id="ups" name="shippingType" value="UPS" type="radio" class="demo_shipping-type custom-control-input" required>
+                            <label class="custom-control-label" for="ups">UPS (<?= UPS_SHIPPING_COST . ' ' . EURO ?>)</label>
+                        </div>
+                    </div>
+
+                    <hr class="mb-4">
+
+                    <h4 class="mb-3">Payment</h4>
+
+                    <div class="d-block my-3">
+                        <div class="custom-control custom-radio">
+                            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+                            <label class="custom-control-label" for="credit">Credit card</label>
+                        </div>
+
+                        <div class="custom-control custom-radio">
+                            <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
+                            <label class="custom-control-label" for="debit">Debit card</label>
+                        </div>
+
+                        <div class="custom-control custom-radio">
+                            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+                            <label class="custom-control-label" for="paypal">PayPal</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="cc-name"><b>Name on card *</b></label>
+                            <input type="text" class="form-control" id="cc-name" placeholder="" required>
+                            <small class="text-muted">Full name as displayed on card</small>
+                            <div class="invalid-feedback">
+                                Name on card is required
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="cc-number"><b>Credit card number *</b></label>
+                            <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                            <div class="invalid-feedback">
+                                Credit card number is required
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-3 mb-3">
+                            <label for="cc-expiration"><b>Expiration *</b></label>
+                            <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                            <div class="invalid-feedback">
+                                Expiration date required
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 mb-3">
+                            <label for="cc-cvv"><b>CVV *</b></label>
+                            <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                            <div class="invalid-feedback">
+                                Security code required
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="mb-4">
+
+                    <button class="btn btn-primary btn-lg btn-block" type="submit">Place the Order</button>
+                </form>
+            </div>
+        </div>
+
+        <style>
+            .lh-condensed {
+                line-height: 1.25;
+            }
+        </style>
+
+        <script>
+            // Disable form submissions if there are invalid fields
+            (function () {
+              'use strict'
+
+              window.addEventListener('load', function () {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation')
+
+                // Loop over them and prevent submission
+                Array.prototype.filter.call(forms, function (form) {
+                  form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                      event.preventDefault()
+                      event.stopPropagation()
+                    }
+                    
+                    form.classList.add('was-validated')
+                  }, false)
+                })
+              }, false)
+            }())
+        </script>
+
+        <!--  
         <div class="row">
             <div class="col-md-7 col-lg-7">
                 <h4>Shipping details</h4> 
@@ -23,8 +271,7 @@
                     Wallet Balance 
                    
                     <span class="badge badge-light"> 
-                       <?= EURO ?> 
-                       <span class="demo_checkout-wallet-balance"><?= $data["user_wallet_balance"]; ?></span>
+                       <span class="demo_checkout-wallet-balance"><?= $data["user_wallet_balance"]; ?></span> <?= EURO ?>
                    </span> 
                 </button>
 
@@ -33,15 +280,15 @@
                 <table class="table table-striped demo_cart-summary">
                     <tr>
                         <td>Cost of Products</td> 
-                        <td><?= EURO ?> 
-                            <span class="demo_checkout-product-cost"><?= $data["cart_cost"]; ?></span> 
+                        <td> 
+                            <span class="demo_checkout-product-cost"><?= $data["cart_cost"]; ?></span> <?= EURO ?>
                         </td>
                     </tr>
 
                     <tr>
-                        <td> Shipping Cost </td>
-                        <td> <?= EURO ?> 
-                            <span class="demo_checkout-shipping-cost"></span> 
+                        <td>Shipping Cost</td>
+                        <td>
+                            <span class="demo_checkout-shipping-cost"></span> <?= EURO ?>
                         </td>
                     </tr>
 
@@ -51,14 +298,14 @@
                         </td>
                         
                         <td> 
-                            <?= EURO ?> 
-                            <span class="demo_checkout-gross-total"></span> 
+                            <span class="demo_checkout-gross-total"></span> <?= EURO ?>
                         </td>
                     </tr>
                 </table>
 
                 <button type="submit" class="btn btn-primary demo_checkout-btn">Checkout Now</button>
             </div>
-        </div>
+        </div> 
+        -->
     </div>
 </div>
